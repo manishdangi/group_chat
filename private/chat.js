de2b9d66f0e57935b083f19a5f5508d0f16edc10
Manish <m.k.dangi99@gmail.com>
2017-01-05 17:43:03 +0530

    first ask the permission by admin to let user join the room

--- private/chat.js
+++ private/chat.js
@@ -1,15 +1,33 @@
 var chat = {};
-chat.room_join = function(io)
+chat.room_join = function(io ,admin_id)
 {
-
 	var connect = io.of('/chat');
+	console.log(admin_id);
 	connect.on('connection',function(socket){
-		socket.on('room_join',function(value){
+		socket.on('is_room_join_permission',function(value){
 			msg = JSON.parse(value);
-			socket.join(msg.room_no);
-			connect.to(msg.room_no).emit('room_join_success',
-				JSON.stringify({value:true,user:msg.user}));
+			socket.join(admin_id);
+			socket.to(admin_id).broadcast.emit('is_chat_allow_emit_admin',JSON.stringify({
+				user:msg.user
+			}))
 		})
+		socket.on('is_chat_allow_result',function(value){
+			msg  = JSON.parse(value);
+			socket.leave(admin_id);
+			console.log(admin_id + " has left the room");
+			if(msg.permission)
+			{
+				socket.join(msg.room);
+				connect.to(msg.room).emit('room_join_success',JSON.stringify({
+					value:true,user:msg.user
+				}));
+			}
+			else
+			{
+
+				socket.disconnect();
+			}
+		});
 		socket.on('message_sent',function(value){
 			msg = JSON.parse(value);
 			socket.to(msg.room_no).broadcast.emit('message_broadcast',JSON.stringify({
