7147d4c7fa2d3e6eff50ba9fe8d00d35bfd5ea99
Manish <m.k.dangi99@gmail.com>
2017-01-09 18:59:36 +0530

    made some small changes and amde use change the room as soon as it hit '/chat'

--- private/chat.js
+++ private/chat.js
@@ -3,30 +3,29 @@ chat.room_join = function(io, admin_id)
 {
 	var connect = io.of('/chat');
 	connect.on('connection',function(socket){
-		socket.join(admin_id);
+		socket.join(admin_id); 
+		socket.emit("admin_room_join")
+		/*	this admin_id will come from dtabase in actual app as many
+			rooms can be activated in the app on the same time */ 
 		socket.on('room_join_permission',function(value){
 			chat.remove = socket.id;
 			msg = JSON.parse(value);
-			socket.to(admin_id).emit('is_chat_allow_emit_admin',JSON.stringify({
+			socket.to(admin_id).emit('is_chat_allow_admin',JSON.stringify({
 				user:msg.user
 			}))						
 		})
 		socket.on('is_chat_allow_result',function(value){
 			msg  = JSON.parse(value);
+			delete io.nsps['/chat'].adapter.rooms[admin_id].sockets[chat.remove];
 			if(msg.permission)
 			{
-				socket.to(admin_id).emit("is_chat_allow_result_emit_admin",JSON.stringify({
+				socket.to(admin_id).emit("chat_allow_result_to_admin",JSON.stringify({
 					result:true
-				}));
-				delete io.nsps['/chat'].adapter.rooms[admin_id].sockets[chat.remove];
-				socket.join(msg.room);
-				connect.to(msg.room).emit('room_join_success',JSON.stringify({
-					value:true,user:msg.user
 				}));
 			}
 			else
 			{
-				socket.to(admin_id).emit("is_chat_allow_result_emit_admin",JSON.stringify({
+				socket.to(admin_id).emit("chat_allow_result_to_admin",JSON.stringify({
 					result:false
 				}));
 				socket.disconnect();
